611bb2754b03bb7198b4ae5f6034d9424c46e37e
Rene <1546500177@qq.com>
2022-01-03 13:48:45 +0800

    更新配置,配置本地化

--- provider/src/main/java/com/rjf/provider/TestController.java
+++ provider/src/main/java/com/rjf/provider/TestController.java
@@ -1,12 +1,14 @@
 package com.rjf.provider;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/index")
+    @SentinelResource("/index")
     public String index(){
         return "Provider !";
     }
